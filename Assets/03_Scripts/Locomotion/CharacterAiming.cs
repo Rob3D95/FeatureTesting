f104465a80855df9a841745b809547cebc1e5249
RobScholten <Robbert.scholten95@gmail.com>
2021-06-25 21:45:59 +0200

    Aiming Added

--- Assets/03_Scripts/Locomotion/CharacterAiming.cs
+++ Assets/03_Scripts/Locomotion/CharacterAiming.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class CharacterAiming : MonoBehaviour
 {
 
     float turnSpeed = 15f;
     Camera cam;
+    public Rig aimLayer;
+    public float aimDuration = 0.3f;
 
     void Start()
     {
@@ -21,5 +24,16 @@ public class CharacterAiming : MonoBehaviour
         float yawCamera = cam.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
 
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        } else
+        {
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+        }
     }
 }
