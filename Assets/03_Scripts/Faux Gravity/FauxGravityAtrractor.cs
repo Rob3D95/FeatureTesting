c2884abaff7545560b8fd67e062964e6ce2a519c
RobScholten <robbert.scholten95@gmail.com>
2021-07-04 21:25:54 +0200

    Added FauxGravity rotation and position

---
+++ Assets/03_Scripts/Faux Gravity/FauxGravityAtrractor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityAtrractor : MonoBehaviour
+{
+
+    [SerializeField]
+    private float gravity = -10f;
+    public void Attract(Transform body, Rigidbody rigidbody)
+    {
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+
+        rigidbody.AddForce(gravityUp * gravity);
+
+
+
+
+        Quaternion targetRot = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+
+
+        
+        body.rotation = Quaternion.Slerp(body.rotation, targetRot, 0.1f);
+
+
+    }
+}
